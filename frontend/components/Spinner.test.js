8135b971713d6130a764e824b6eeb9da567f4ed1
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-06-02 17:47:07 -0400

    added spinner test

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,32 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render } from "@testing-library/react";
+import Spinner from "./Spinner";
+// Import the Spinner component into this file and test
+// that it renders what it should for the different props it can take.
+
+let spinnerOn = true;
+
+beforeEach(() => {
+  render(<Spinner on={spinnerOn} />);
+});
+
+afterEach(() => {
+  window.localStorage.clear();
+});
+
+describe("Spinner component", () => {
+  test("sanity", () => {
+    expect(true).toBe(true);
+  });
+
+  test("Spinner has no errors when true", async () => {
+    render(<Spinner on={spinnerOn} />);
+  });
+
+  test("Spinner has no errors when false", async () => {
+    spinnerOn = false;
+    render(<Spinner on={spinnerOn} />);
+  });
+});
